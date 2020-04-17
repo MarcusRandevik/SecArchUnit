daed2649161d43bfd2a9ef4982fd1ef2c488fb60
Marcus Randevik <marcus@randevik.nu>
2020-04-17 21:15:11 +0200

    Sample code

--- Validation/sonarcustomrules/src/main/java/customrules/checks/LogSecurityEventsRule.java
+++ Validation/sonarcustomrules/src/main/java/customrules/checks/LogSecurityEventsRule.java
@@ -3,11 +3,12 @@ package customrules.checks;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.tree.Tree;
 
+import java.util.Collections;
 import java.util.List;
 
 public class LogSecurityEventsRule extends IssuableSubscriptionVisitor {
     @Override
     public List<Tree.Kind> nodesToVisit() {
-        return null;
+        return Collections.emptyList();
     }
 }
