15970c02710b7be582e4787391196c891a11eab3
SomewhatDisoriented <github@podd.xyz>
2020-04-17 13:01:28 +0200

    Apply constraints in SecArchUnit

---
+++ Validation/ATMsimulator/src/SecurityTest.java
@@ -0,0 +1,59 @@
+import org.junit.runner.RunWith;
+
+import com.github.secarchunit.SecArchUnit;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import com.tngtech.archunit.lang.ArchRule;
+
+import atm.physical.CardReader;
+import atm.physical.CashDispenser;
+import atm.physical.EnvelopeAcceptor;
+import atm.physical.Log;
+import atm.physical.NetworkToBank;
+import atm.transaction.Transaction;
+
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = {"", "atm"})
+public class SecurityTest {
+	// -- BASIC CONSTRAINTS --
+	
+	@ArchTest
+    public static final ArchRule logSecurityEvents = SecArchUnit.logSecurityEvents(
+    		JavaClass.Predicates.belongToAnyOf(
+	    		CardReader.class,
+	    		CashDispenser.class,
+	    		EnvelopeAcceptor.class,
+	    		NetworkToBank.class,
+	    		Transaction.class
+	    	), Log.class);
+	
+	@ArchTest
+    public static final ArchRule enforceAuthenticationAtCentralPoint =
+    		SecArchUnit.enforceAuthenticationAtCentralPoint(
+    				Transaction.class,
+    				Transaction.class);
+	
+	@ArchTest
+    public static final ArchRule enforceAuthorizationAtCentralPoint =
+    		SecArchUnit.enforceAuthorizationAtCentralPoint(
+    				Transaction.class,
+    				Transaction.class);
+	
+	@ArchTest
+    public static final ArchRule sendMessagesFromCentralPoint =
+    		SecArchUnit.sendOutboundMessagesFromCentralPoint(
+		    		NetworkToBank.class,
+		    		JavaClass.Predicates.belongToAnyOf(Transaction.class));
+    
+	// -- ANNOTATION CONSTRAINTS --
+
+    @ArchTest
+    public static final ArchRule validateUserInput =
+    		SecArchUnit.validateUserInput();
+
+    @ArchTest
+    public static final ArchRule limitResourceAllocation =
+    		SecArchUnit.limitResourceAllocation();
+}
