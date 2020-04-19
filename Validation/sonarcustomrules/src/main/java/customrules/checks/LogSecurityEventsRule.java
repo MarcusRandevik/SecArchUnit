c5eabad75fe819af4e70d46a3020c903cb3c9525
Marcus Randevik <marcus@randevik.nu>
2020-04-19 16:08:57 +0200

    inplemented first constraint

--- Validation/sonarcustomrules/src/main/java/customrules/checks/LogSecurityEventsRule.java
+++ Validation/sonarcustomrules/src/main/java/customrules/checks/LogSecurityEventsRule.java
@@ -1,14 +1,65 @@
 package customrules.checks;
 
+import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.cfg.ControlFlowGraph;
+import org.sonar.plugins.java.api.semantic.MethodMatchers;
+import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
 import org.sonar.plugins.java.api.tree.Tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+@Rule(
+        key = "LogSecurityEvents",
+        name = "All security events must be logged",
+        description = "Public methods within a security service class must call a logger"
+)
+
 public class LogSecurityEventsRule extends IssuableSubscriptionVisitor {
+
+    public static String LOGGER_CLASS_TEST = "java.math.BigDecimal";
+    public static String LOGGER_CLASS = "java.math.BigDecimal";
+    public static List<String> SECURITY_CLASSES = new ArrayList<String>(Arrays.asList("LogSecurityEventsCheck".toLowerCase(),
+                                                                                    "CardReader".toLowerCase()));
+
+    MethodMatchers loggerMethodsTest = MethodMatchers.create().ofTypes(LOGGER_CLASS_TEST).anyName().withAnyParameters().build();
+    MethodMatchers loggerMethods = MethodMatchers.create().ofTypes(LOGGER_CLASS).anyName().withAnyParameters().build();
+
     @Override
     public List<Tree.Kind> nodesToVisit() {
-        return Collections.emptyList();
+        return Collections.singletonList(Tree.Kind.METHOD);
+    }
+
+    @Override
+    public void visitNode(Tree tree) {
+        MethodTree method = (MethodTree) tree;
+
+        String methodEnclosingClass = method.symbol().enclosingClass().name().toLowerCase();
+        if (!SECURITY_CLASSES.contains(methodEnclosingClass)) return;
+
+        boolean containsCallToLogger = false;
+
+        ControlFlowGraph cfg = method.cfg();
+
+        for (ControlFlowGraph.Block block : cfg.blocks()) {
+            System.out.println(block.elements().size());
+            for (Tree blockTree : block.elements()) {
+                if (blockTree.is(Tree.Kind.METHOD_INVOCATION)) {
+                    MethodInvocationTree mit = (MethodInvocationTree) blockTree;
+                    if (loggerMethodsTest.matches(mit) || loggerMethods.matches(mit)) {
+                        containsCallToLogger = true;
+
+                    }
+                }
+            }
+        }
+
+        if (!containsCallToLogger) {
+            reportIssue(method.simpleName(), "Secure classes must contain call to logger");
+        }
     }
 }
--- Validation/sonarcustomrules/src/test/files/LogSecurityEventsCheck.java
+++ Validation/sonarcustomrules/src/test/files/LogSecurityEventsCheck.java
@@ -1,9 +1,13 @@
-class LogSecurityEventsCheck {
-    int     foo1() { return 0; }
-    void    foo2(int value) { }
-    int     foo3(int value) { return 0; } // Noncompliant
-    Object  foo4(int value) { return null; }
+import java.math.BigDecimal;
 
-    int     foo6(int value, String name) { return 0; }
-    int     foo7(int ... values) { return 0;}
+class LogSecurityEventsCheck {
+    public void foo1()  // Noncompliant
+    {
+
+    }
+
+    public void foo2()
+    {
+        BigDecimal.valueOf(0);
+    }
 }
