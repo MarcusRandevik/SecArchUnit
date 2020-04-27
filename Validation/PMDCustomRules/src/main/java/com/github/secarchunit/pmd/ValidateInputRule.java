b9ac006a4066094a7781f8d22cd9f37aaabbe74f
SomewhatDisoriented <github@podd.xyz>
2020-04-27 16:38:06 +0200

    Implement constraint 4

--- Validation/PMDCustomRules/src/main/java/com/github/secarchunit/pmd/ValidateInputRule.java
+++ Validation/PMDCustomRules/src/main/java/com/github/secarchunit/pmd/ValidateInputRule.java
@@ -3,8 +3,11 @@ package com.github.secarchunit.pmd;
 import net.sourceforge.pmd.lang.java.ast.ASTMethodDeclaration;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
+import java.util.List;
+
 public class ValidateInputRule extends AbstractJavaRule {
     private static final String USER_INPUT = "com.github.secarchunit.concepts.UserInput";
+    private static final String INPUT_VALIDATOR = "com.github.secarchunit.concepts.InputValidator";
 
     public ValidateInputRule() {
         super();
@@ -16,17 +19,28 @@ public class ValidateInputRule extends AbstractJavaRule {
     @Override
     public Object visit(ASTMethodDeclaration method, Object data) {
         if (method.isAnnotationPresent(USER_INPUT)) {
-            //System.out.println("Found @UserInput on " + method);
-        }
+            // Check for in-line validation
+            if (method.isAnnotationPresent(INPUT_VALIDATOR)) {
+                // Checks out
+                return data;
+            }
 
-        /*
-        boolean callsValidator = Util.getMethodCallsFrom(method).stream()
-                .anyMatch(call -> LOGGER.equals(call.targetOwner));
+            // Check for calls to validator
+            List<String> inputValidators = AnnotationHelper.getAnnotations(INPUT_VALIDATOR);
+            boolean callsValidator = Util.getMethodCallsFrom(method).stream()
+                    .anyMatch(call -> inputValidators.contains(call.targetOwner)
+                            || inputValidators.contains(call.targetOwner + "." + call.target));
 
-        if (!callsLogger) {
+            if (callsValidator) {
+                // Checks out
+                return data;
+            }
+
+            // Check if all callers of this method are input validators
+            // TODO Can't be done in PMD; assume no validation occurs
+
             addViolation(data, method);
         }
-        */
 
         return data;
     }
